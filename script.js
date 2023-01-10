44358c8f7acc16ae7823a55c3c5338f2df52fba4
John W <perseo1326@gmail.com>
2023-01-10 19:02:41 +0100

    apertura, Carga y verificacion del archivo

--- script.js
+++ script.js
@@ -1,35 +1,67 @@
 
-/*
-const texto = document.getElementById("file");
-console.log(file);
 
+        function processFile(file) {
 
-/*
-function leerArchivo(e) {
-    let archivo = null;
-    archivo = e.target.files[0];
-    console.log(archivo);
-    if (!archivo) {
-        alert("Problemas cargando el archivo");
-        return;
-    }
-    var lector = new FileReader();
-    lector.onload = function(e) {
-        var contenido = e.target.result;
-        mostrarContenido(contenido);
-    };
-    lector.readAsText(archivo);
-    alert("Recuerde Esperar hasta que se cargue el archivo");
-    console.log("Archivo Cargado");
-}
+            const aFile = openTextFile(file);
+            if(!aFile) {            
+                return;
+            }
+            // console.log("Propiedades del archivo:");
+            // console.log(aFile);
+            loadTextFile(aFile);
+        }
 
-function mostrarContenido(contenido) {
-    var elemento = document.getElementById('contenido-archivo');
-    elemento.innerHTML = contenido;
-    // alert("mostrar cont");
-}
+        // *********************************************************
+        function openTextFile(file) {
+            // Apertura del archivo seleccionado
+            let aFile = file.target.files[0];
+            
+            if (!aFile) {
+                alert("No se ha seleccionado ningun archivo.");
+                console.log("No se ha seleccionado ningun archivo.");
+                return null;
+            }
+            /*
+            if (aFile.type && !aFile.type.startsWith('text/')) {
+                alert('El archivo seleccionado NO es válido.');
+                console.log('El archivo seleccionado NO es válido.');
+                return null;
+            }
+            */
+            return aFile;
+        }
 
-document.getElementById('file-input')
-    .addEventListener('change', leerArchivo, false);
+        // *********************************************************
+        function loadTextFile(aFile) {
+            // Carga del contenido del archivo
+            let lector = new FileReader();
+            lector.onload = function(aFile) {
+                let content = new Array;
+                let rows = aFile.target.result.split("\n");
+                rows.forEach(row => {
+                    let columns = row.split("\t"); 
+                    content.push(columns);    
+                });
 
-*/
+                processContent(content);
+                showContent(content);
+            };
+            lector.readAsText(aFile);
+        }
+
+        // *********************************************************
+        function processContent(content) {
+            console.log(content);
+
+            
+        }
+        // *********************************************************
+        function showContent(content) {
+            let element = document.getElementById('contenido-archivo');
+            element.innerHTML = content;
+        }
+
+        // *********************************************************
+        document.getElementById('file-input')
+        .addEventListener('change', processFile, false);
+        
